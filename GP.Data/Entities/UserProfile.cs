231d535b9f2162f494dc7da0b38efa3585cddd6f
Soha Refaat <144360872+soha-Refaat@users.noreply.github.com>
2025-04-24 21:31:14 +0200

    Add files via upload
    User Profile

---
+++ GP.Data/Entities/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP.Data.Entities;
+
+ public class UserProfile
+    {
+        [Display(Name = "Full Name")]
+        public string? FullName { get; set; }
+
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+
+        public byte[]? Image { get; set; }
+    }
+
